b86ffe469d879115c55577cea51f39bb93a289b7
ryof <ryo.furuyama@gmail.com>
2020-02-12 16:40:42 +0900

    Add dynamic tepmlate splitter for customizer

--- customizer/customizer.rb
+++ customizer/customizer.rb
@@ -16,15 +16,27 @@ Dir.glob('**/*.y*ml', base: template_dir) do |template_file|
 
   template = File.read(template_file)
   # YAML.load only loads first document separated by '---'
-  # Splitting template body for its intrinsic functions to be eliminated.
+  # Splitting template body for its intrinsic functions not to be eliminated.
   # ex) !Ref will be eliminated as it's loaded in YAML.load
+  #
+  # Though this separator should really be inserted in the template,
+  # it's impossibel to suppress E0000 error in cfn-lint because of a bug.
+  # cf) https://github.com/aws-cloudformation/cfn-python-lint/issues/1133
+  # Therefore seprator is inserted here dynamically.
+  separater_index = 0
+  template.each_line.with_index do |line, index|
+    if /^(Conditions|Resources):/ =~ line
+      separater_index = index
+      break
+    end
+  end
+  template = template.lines.insert(separater_index, "---\n").join
   template_head = YAML.load(template)
   overridden = template_head.deep_merge!(overriding_object)
   dirname = FileUtils.mkdir_p(File.join(out_dir, File.dirname(template_file)))
   output_path = File.join(dirname, File.basename(template_file))
   File.write(output_path, YAML.dump(overridden))
-  lines = template.lines
   File.open(output_path, 'a') do |f|
-    lines[lines.index("---\n")..-1].each { |line| f.print line }
+    template.lines[separater_index + 1..-1].each { |line| f.print line }
   end
 end
